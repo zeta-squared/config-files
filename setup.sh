54e035c966b0c2c34e34ac874518a0903d03d72d
zeta-squared <zeaiter.zeaiter@hotmail.com>
2025-05-09 21:31:47 +1000

    refactor: changed order in setup script

--- setup.sh
+++ setup.sh
@@ -6,20 +6,39 @@ echo "Installing curL..."
 sudo apt install curl
 echo "Successfully installed curL."
 
-echo "Downloading neovim..."
-FILE='nvim-linux-x86_64'
-curl -o ${DOTFILES_DIR}/${FILE}.tar.gz -L https://github.com/neovim/neovim/releases/download/v0.11.1/${FILE}.tar.gz
-echo "Extracting tarball..."
-tar xzf ${DOTFILES_DIR}/${FILE}.tar.gz -C ${DOTFILES_DIR}
-echo "Installing neovim..."
-sudo cp -rf ${DOTFILES_DIR}/${FILE}/bin/* /usr/local/bin/
-sudo cp -rf ${DOTFILES_DIR}/${FILE}/lib/* /usr/local/lib/
-sudo cp -rf ${DOTFILES_DIR}/${FILE}/share/* /usr/local/share/
-echo "Installing dependencies for neovim..."
-sudo apt install xclip
-echo "Cleaning up..."
-rm -rf ${DOTFILES_DIR}/${FILE}*
-echo "Successfully installed neovim."
+echo "Installing zathura..."
+sudo apt install zathura
+echo "Successfully installed zathura."
+
+echo "Installing yakuake..."
+sudo apt install yakuake
+echo "Successfully installed yakuake."
+
+echo "Installing gh..."
+sudo apt install gh
+echo "Successfully installed gh."
+
+echo "Installing kvantum..."
+sudo apt install qt5-style-kvantum
+echo "Successfully installed kvantum."
+
+echo "Install GCC..."
+sudo apt install gcc
+echo "Successfully installed gcc."
+
+echo "Installing python3-venv..."
+sudo apt install python3-venv
+echo "Successfully installed python3-venv."
+
+echo "Installing node with pnpm..."
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+\. "$HOME/.nvm/nvm.sh"
+nvm install 22
+node -v # Should print "v22.15.0".
+nvm current # Should print "v22.15.0".
+corepack enable pnpm
+pnpm -v
+echo "Successfully installed node with pnpm."
 
 echo "Downloading ripgrep..."
 FILE='ripgrep-14.1.1-x86_64-unknown-linux-musl'
@@ -43,45 +62,6 @@ echo "Cleaning up..."
 rm -rf ${DOTFILES}/fzf-*
 echo "Successfully installed fzf."
 
-echo "Installing vim-plug..."
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-echo "Vim-plug has been installed."
-
-echo "Installing node with pnpm..."
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-\. "$HOME/.nvm/nvm.sh"
-nvm install 22
-node -v # Should print "v22.15.0".
-nvm current # Should print "v22.15.0".
-corepack enable pnpm
-pnpm -v
-echo "Successfully installed node with pnpm."
-
-echo "Installing zathura..."
-sudo apt install zathura
-echo "Successfully installed zathura."
-
-echo "Installing yakuake..."
-sudo apt install yakuake
-echo "Successfully installed yakuake."
-
-echo "Installing gh..."
-sudo apt install gh
-echo "Successfully installed gh."
-
-echo "Installing kvantum..."
-sudo apt install qt5-style-kvantum
-echo "Successfully installed kvantum."
-
-echo "Install GCC..."
-sudo apt install gcc
-echo "Successfully installed gcc."
-
-echo "Installing python3-venv..."
-sudo sudo apt install python3-venv
-echo "Successfully installed python3-venv."
-
 echo "Downloading lazygit..."
 curl -o ${DOTFILES_DIR}/lazygit-0.50.0_Linux_x86_64.tar.gz -L https://github.com/jesseduffield/lazygit/releases/download/v0.50.0/lazygit_0.50.0_Linux_x86_64.tar.gz
 echo "Extracting tarball..."
@@ -98,6 +78,31 @@ cp -rf ${DOTFILES_DIR}/config/* ${HOME}/.config/
 cp -rf ${DOTFILES_DIR}/local/* ${HOME}/.local/share/
 echo "Successfully installed dotfiles."
 
+echo "Downloading neovim..."
+FILE='nvim-linux-x86_64'
+curl -o ${DOTFILES_DIR}/${FILE}.tar.gz -L https://github.com/neovim/neovim/releases/download/v0.11.1/${FILE}.tar.gz
+echo "Extracting tarball..."
+tar xzf ${DOTFILES_DIR}/${FILE}.tar.gz -C ${DOTFILES_DIR}
+echo "Installing neovim..."
+sudo cp -rf ${DOTFILES_DIR}/${FILE}/bin/* /usr/local/bin/
+sudo cp -rf ${DOTFILES_DIR}/${FILE}/lib/* /usr/local/lib/
+sudo cp -rf ${DOTFILES_DIR}/${FILE}/share/* /usr/local/share/
+echo "Installing dependencies for neovim..."
+sudo apt install xclip
+npm i -g tree-sitter-cli
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+echo "Creating python provider for neovim..."
+python3 -m venv ${HOME}/.local/share/nvim/nvim_venv
+source ${HOME}/.local/share/nvim/nvim_venv/bin/activate
+pip install pynvim
+deactive
+echo "Running plugin installation..."
+/usr/local/bin/nvim -c PlugInstall -c q -c q
+echo "Cleaning up..."
+rm -rf ${DOTFILES_DIR}/${FILE}*
+echo "Successfully installed neovim."
+
 echo "Installing fonts..."
 mkdir -p ${HOME}/.local/share/fonts/
 curl -o ${HOME}/.local/share/fonts/SourceCodePro.tar.xz\
@@ -111,17 +116,6 @@ echo "Installing system cursors and sddm theme..."
 sudo cp -rf ${DOTFILES_DIR}/icons/* /usr/share/icons/
 sudo cp -rf ${DOTFILES_DIR}/sddm/* /usr/share/sddm/themes/
 echo "Successfully installed system cursors and sdd theme."
-
-echo "Creating python provider for neovim..."
-python3 -m venv ${HOME}/.local/share/nvim/nvim_venv
-source ${HOME}/.local/share/nvim/nvim_venv/bin/activate
-pip install pynvim
-deactive
-echo "Python provider setup complete."
-
-echo "Running plugin installation in neovim..."
-/usr/local/bin/nvim -c PlugInstall -c q -c q
-echo "Successfully installed neovim plugins."
 
 echo "Installing Firefox..."
 sudo add-apt-repository ppa:mozillateam/ppa
@@ -151,7 +145,5 @@ echo \
 sudo apt-get update
 echo "Installing docker..."
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-echo "Configuring group to manage docker as non-root user..."
-sudo usermod -aG docker $USER
-newgrp docker
+echo "Configuring group to manage docker as non-root user visit https://docs.docker.com/engine/install/linux-postinstall/"
 echo "Successfully installed docker."
