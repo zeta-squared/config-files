2b4bfc295ebd74e98267c13f91b6c426a67aec99
zeta-squared <zeaiter.zeaiter@hotmail.com>
2025-06-13 19:18:25 +1000

    chore: disabled eslint on save

--- config/nvim/lua/zeta-squared/lsp.lua
+++ config/nvim/lua/zeta-squared/lsp.lua
@@ -118,23 +118,25 @@ require('mason-tool-installer').setup({
 })
 
 for _, settings in pairs(servers) do
-    if settings.lsp == 'eslint' then
-        local base_on_attach = vim.lsp.config.eslint.on_attach
-        settings.config.on_attach = function(client, bufnr)
-            if not base_on_attach then return end
+    vim.lsp.config(settings.lsp, settings.config)
+    vim.lsp.enable(settings.lsp)
+    -- if settings.lsp == 'eslint' then
+    --     local base_on_attach = vim.lsp.config.eslint.on_attach
+    --     settings.config.on_attach = function(client, bufnr)
+    --         if not base_on_attach then return end
 
-            base_on_attach(client, bufnr)
-            vim.api.nvim_create_autocmd('BufWritePre', {
-                buffer = bufnr,
-                command = 'LspEslintFixAll',
-            })
-        end
-        vim.lsp.config(settings.lsp, settings.config)
-        vim.lsp.enable(settings.lsp)
-    else
-        vim.lsp.config(settings.lsp, settings.config)
-        vim.lsp.enable(settings.lsp)
-    end
+    --         base_on_attach(client, bufnr)
+    --         vim.api.nvim_create_autocmd('BufWritePre', {
+    --             buffer = bufnr,
+    --             command = 'LspEslintFixAll',
+    --         })
+    --     end
+    --     vim.lsp.config(settings.lsp, settings.config)
+    --     vim.lsp.enable(settings.lsp)
+    -- else
+    --     vim.lsp.config(settings.lsp, settings.config)
+    --     vim.lsp.enable(settings.lsp)
+    -- end
 end
 
 -- Diagnostic Config
